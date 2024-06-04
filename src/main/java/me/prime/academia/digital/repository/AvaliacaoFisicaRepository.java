b28d5e4a9fa4534e9f20a3d4d065986a5a603373
Dnakalfa <137450842+Dnakalfa@users.noreply.github.com>
2024-06-04 14:05:32 -0300

    novo projeto

---
+++ src/main/java/me/prime/academia/digital/repository/AvaliacaoFisicaRepository.java
@@ -0,0 +1,10 @@
+package me.prime.academia.digital.repository;
+
+import me.prime.academia.digital.entity.AvaliacaoFisica;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AvaliacaoFisicaRepository extends JpaRepository<AvaliacaoFisica, Long> {
+
+}
